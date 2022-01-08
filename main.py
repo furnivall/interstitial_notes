5bb6885ad4e5a2bc44b074b3b9233eba3258c6e5
furnivall <danielfurnivall@gmail.com>
2022-01-08 22:21:02 +0000

    Amended function names for pep-8

--- main.py
+++ main.py
@@ -3,30 +3,31 @@ from datetime import datetime
 from pathlib import Path
 import sys
 import subprocess
-from termcolor import colored, cprint
+from termcolor import cprint
 
 # create folder for logs if doesn't exist
 dirname = str(Path.home()) + "/interstit"
 
 if not os.path.isdir(dirname):
     os.mkdir(dirname)
-    cprint(f'Directory does not exist {dirname}','red')
+    cprint(f'Directory does not exist {dirname}', 'red')
 
 # get date for filename
 today = datetime.now().strftime('%d-%m-%y')
 curr_time = datetime.now().strftime('%H:%M')
 
-
-
-
 filename = dirname + '/' + today + '.html'
+
+# get args from cmdline
 content = " ".join(sys.argv[2:])
+
 
 def if_file_not_exist():
     # if today's file does not exist, create and add header
     if not os.path.isfile(filename):
-        cprint(f"File for today does not yet exist.",'magenta')
+        cprint(f"File for today does not yet exist.", 'magenta')
         with open(filename, 'w') as f:
+            # add css and headers
             f.write("<head>"
                     "<style>"
                     "h1{font-family:'arial'; "
@@ -58,26 +59,29 @@ def if_file_not_exist():
             cprint(f'File created at \u001b]8;;{filename}\u001b\\{filename}\u001b]8;;\u001b\\', 'magenta')
 
 
-def appendNote():
+def append_note():
     with open(filename, 'a') as f:
         f.write(f'<p><span class="timestamp">{curr_time}:</span> {content}</p>')
     cprint(f'Note added - "{content}"', 'cyan')
 
-def appendTodo():
+
+def append_todo():
     with open(filename, 'a') as f:
         f.write(f'<label><span class="timestamp">{curr_time}:</span> {content}<input type="checkbox"/></label>')
     cprint(f'Todo added - "{content}"', 'blue')
 
 
-def openFile():
+def open_file():
     try:
         os.startfile(filename)
     except:
         subprocess.call(['open', filename])
 
+
 def bad_args():
     cprint(f'Sorry, the arguments you have provided are incorrect. Try using "todo" or "time" as your initial argument.'
            'red')
+
 
 def help_text():
     cprint(f'Interstit - a timestamped note-taking application for the command line developed by Danny Furnivall.',
@@ -87,19 +91,18 @@ def help_text():
     cprint(f'"todo <your text here>" - this creates a todo item within the note file for today', 'yellow')
     cprint(f'"open" - this opens the file for today in your default browser', 'blue')
 
+
 if len(sys.argv) > 1:
     if_file_not_exist()
     if sys.argv[1] == 'note':
-        appendNote()
+        append_note()
     elif sys.argv[1] == 'todo':
-        appendTodo()
+        append_todo()
     elif sys.argv[1] == 'open':
-        openFile()
+        open_file()
     elif sys.argv[1] == 'help':
         help_text()
     else:
         bad_args()
 else:
     help_text()
-
-
